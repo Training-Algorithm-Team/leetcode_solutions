14e862324baf4cc6a05e59beab68bd57f3693c1b
nguyenhieu008 <nguyenhieu008@gmail.com>
2021-01-08 17:57:15 +0700

    Update [E]69.cpp

--- [E]69.cpp
+++ [E]69.cpp
@@ -4,15 +4,20 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        // cout << (int)sqrt(INT_MAX) << endl;
-        // I must cout to find the max of range first, before searching.
-        int l = 0, r = 46340;
+        int l = 0, r = x;
         while (l <= r) {
-            int mid = (l + r) / 2;
-            int p = mid * mid;
-            if (p < x) l = mid + 1;
-            else if (p > x) r = mid - 1;
-            else return mid;
+            // cout << "l = " << l << " r = " << r << endl;
+            long mid = l + (r - l) / 2;
+            long p = mid * mid;
+            if (p > x) {
+                r = mid - 1;
+            }
+            else if (p < x) {
+                l = mid + 1;
+            }
+            else {
+                return mid;
+            }
         }
         return r;
     }
