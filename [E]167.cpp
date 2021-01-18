7bd652691fe4f26c75c2a5df8e741c0485a5b695
nguyenhieu008 <nguyenhieu008@gmail.com>
2021-01-18 10:07:40 +0700

    Update [E]167.cpp

--- [E]167.cpp
+++ [E]167.cpp
@@ -7,8 +7,37 @@
 - Range search sẽ từ [i + 1, size) để tránh bị trùng với i.
 - Độ phức tạp O(n * log(n))
 - Có thể dùng hash map để giảm độ phức tạp xuống O(n).
+
+- Ngoài ra, có thể có thêm 1 cách khác, sử dụng kĩ thuật sliding windows - 2 pointers để xử lý:
+    + Ban đầu, ta lấy tổng của phần tử đầu và phần tử cuối của dãy rồi so sánh với target, gọi là i và j tương ứng với đầu và cuối.
+    + Ta có nhận xét nếu tổng này > target => Mọi phần tử sau i nếu cộng với j thì đều khiền cho tổng tăng lên => không thể có sum = target. Do đó ta chỉ cần cho j--
+    để khiến tổng tạo ra có khả năng = target và sau đó thử tiếp.
+    + Tương tự với trường hợp < target => Ta cần cho i++.
 */
 
+// C1: Binary search.
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        for (int i = 0; i < numbers.size() - 1 && numbers[i] * 2 <= target; ++i) {
+            int l = i + 1, r = numbers.size();
+            while (r - l > 1) {
+                int mid = l + (r - l) / 2;
+                if (numbers[mid] + numbers[i] <= target) {
+                    l = mid;
+                } else {
+                    r = mid;
+                }
+            }
+            if (numbers[l] + numbers[i] == target) {
+                return {i + 1, l + 1};
+            }
+        }
+        return {-1, -1};
+    }
+};
+
+// C2: 2 pointers
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
