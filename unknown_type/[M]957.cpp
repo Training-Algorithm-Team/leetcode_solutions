b880533cbfd6e81e02d3a6c4626ed891c3e9d297
nguyenhieu008 <nguyenhieu008@gmail.com>
2021-01-29 18:05:48 +0700

    Create [M]957.cpp

---
+++ unknown_type/[M]957.cpp
@@ -0,0 +1,6 @@
+// Problem: 957. Prison Cells After N Days - Medium
+// Link: https://leetcode.com/problems/prison-cells-after-n-days/
+
+/* Nhận xét:
+- Bài này anh chịu thật, chú có hint gì ko?
+*/
