9e466626f2012306228daf603caf091b84617ffb
nguyenhieu008 <nguyenhieu008@gmail.com>
2021-02-04 14:51:16 +0700

    Update [M]313.cpp

--- heap/[M]313.cpp
+++ heap/[M]313.cpp
@@ -15,6 +15,8 @@ việc liệt kê tất cả các số với 1 dãy nextUgly[i] là không cần
 - Ta lưu toàn bộ các nextUgly tương ứng với toàn bộ primes. Việc ở mỗi vòng lặp là tìm 1 uglyNumber tiếp theo, ta thực hiện bằng cách chọn nextUgly nhỏ nhất. Việc này gợi ý
 ta có thể dùng heap để giảm thời gian tính toán. Ngoài ra, khi đã thêm mới ugly number, ta cần tìm nextUgly tiếp theo tương ứng với p, việc này thực hiện bằng cách
 ta tăng idx lên 1, sau đó nhân với p, để tìm ugly number tiếp theo trong dãy nextUgly của p.
+- Time complexity: O(n * log(k));
+- Space complexity: O(n)
 */
 
 class Solution {
@@ -26,21 +28,49 @@ public:
         vector<int> ugly(n);
         priority_queue<pip, vector<pip>, greater<pip>> nextUgly;
         
-        ugly[0] = 1;
-        for (int i = 0; i < k; ++i) {
-            nextUgly.push({primes[i], {primes[i], 0}});
+        for (int &p : primes) {
+            nextUgly.push({1, {p, 0}});
         }
         
-        for (int i = 1; i < n; ++i) {
+        for (int i = 0; i < n; ++i) {
             int u = nextUgly.top().first;       
             ugly[i] = u;
             
             while (nextUgly.top().first == u) {
                 auto [p, idx] = nextUgly.top().second;
                 nextUgly.pop();
-                idx++;
-                nextUgly.push({ugly[idx] * p, {p, idx}});
+                nextUgly.push({ugly[idx] * p, {p, idx + 1}});
             }
+        }
+        return ugly[n-1];
+    }
+};
+
+/* C2:
+- Cách này chỉ đơn thuần là duyệt toàn bộ mảng primes, để tìm min chứ ko dùng heap. Độ phức tạp trên lý thuyết là O(n * k) tức là sẽ lớn hơn cách 1. Tuy nhiên khi chạy 
+thực tế thì nhanh hơn rất nhiều: 40ms vs. 120ms. Thậm chí khi anh kiểm tra discussion thì các ngôn ngữ khác đều có hiện tượng này.
+- Có vẻ như cách sử dụng heap có overhead quá lớn dẫn đến khi k tương đối nhỏ thì các hệ số overhead dành cho C*log(k) còn lớn hơn k. Chú nghĩ sao?
+*/
+class Solution {
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        int k = primes.size();
+        vector<int> ugly(n);
+        vector<int> idx(k, 0);
+        vector<int> nextUgly(k, 1);
+        
+        ugly[0] = 1;
+        int next = INT_MAX;
+        for (int i = 1; i < n; ++i) {
+            next = INT_MAX;
+            for (int j = 0; j < k; ++j) {
+                if (ugly[i-1] == nextUgly[j]) {
+                    nextUgly[j] = ugly[idx[j]] * primes[j];
+                    idx[j]++;
+                }
+                next = min(next, nextUgly[j]);
+            }
+            ugly[i] = next;
         }
         return ugly[n-1];
     }
