aed66116d4d38a2f020ab06c712f39c527b84707
nguyenhieu008 <nguyenhieu008@gmail.com>
2021-02-05 09:57:26 +0700

    Update [M]970.cpp

--- hash_table/[M]970.cpp
+++ hash_table/[M]970.cpp
@@ -3,6 +3,9 @@
 
 /* Nhận xét:
 - Bài này anh bị overthinking rồi, anh chả biết nói gì nữa.
+- Time complexity: O(log(bound) * log(bound) * log(log(bound))). Số lượng đáp án có thể là O(log(bound) * log(bound)). Với mỗi đáp án có thể, ta còn phải push thêm phần tử
+mới vào heap, heap có độ lớn là log(bound) => Độ phức tạp thao tác push là log(log(bound)).
+- Space complexity: O(log(bound)).
 */
 
 class Solution {
