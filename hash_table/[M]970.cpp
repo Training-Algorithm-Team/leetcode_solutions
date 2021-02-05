eaef474f4712c3fe3ab9236ac9b1517535704339
nguyenhieu008 <nguyenhieu008@gmail.com>
2021-02-05 09:49:09 +0700

    Update [M]970.cpp

--- hash_table/[M]970.cpp
+++ hash_table/[M]970.cpp
@@ -10,19 +10,13 @@ public:
     typedef pair<int, int> pii;
     typedef pair<int, pii> pip;
     vector<int> powerfulIntegers(int x, int y, int bound) {
-        if (bound < 2) {
-            return {};
-        }
-        if (x == 1 && y == 1) {
-            return {2};
-        }
         priority_queue<pip, vector<pip>, greater<pip>> heap;
 
-        if (y == 1 && x != 1) {
-            swap(x, y);
-        }
         for (int powy = 1; powy + 1 <= bound; powy *= y) {
             heap.push({1 + powy, {1, powy}});
+            if (y == 1) {
+                break;
+            }
         }    
         
         
