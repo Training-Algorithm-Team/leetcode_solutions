a003e333596cf5b2a5e5e54ecb050164c5afc8d1
nguyenhieu008 <nguyenhieu008@gmail.com>
2021-03-10 11:22:45 +0700

    Create [M]652.cpp

---
+++ tree/[M]652.cpp
@@ -0,0 +1,6 @@
+// 652. Find Duplicate Subtrees - Medium
+// https://leetcode.com/problems/find-duplicate-subtrees/
+
+/* Analysis:
+- Bài này anh nghĩ phải dùng DP, nhưng đang chưa biết lưu dữ liệu kiểu gì, chú có gợi ý gì ko?
+*/
