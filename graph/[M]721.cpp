b8c117d96383b4798626e7224ad33ef01928f099
nguyenhieu008 <nguyenhieu008@gmail.com>
2021-03-25 11:27:30 +0700

    Create [M]721.cpp

---
+++ graph/[M]721.cpp
@@ -0,0 +1,7 @@
+// 721. Accounts Merge - Medium
+// https://leetcode.com/problems/accounts-merge/
+
+/* Analysis:
+- Bài này hơi khó, chú có hint gì ko?
+- Anh đang nghĩ đến việc phải build graph rồi dùng DFS để liệt kê cách thành phần liên thông. Nhưng code khi đó hơi phức tạp.
+*/
